07a27c14826815698acb1388c262f950583bda07
Heath <916224+HeathT@users.noreply.github.com>
2022-04-12 23:34:37 -0500

    Starting to work on Drag & Drop funcationality

--- js/testing.js
+++ js/testing.js
@@ -104,3 +104,20 @@ global.textChange = {
         setInterval(() => {this.color('centerText')}, 2000);    // Example of an arrow function
     }
 }
+
+
+global.dd = {
+    allowDrop: function(ev) {
+        ev.preventDefault();
+    },
+      
+    drag: function(ev) {
+        ev.dataTransfer.setData("text", ev.target.id);
+    },
+      
+    drop: function(ev) {
+        ev.preventDefault();
+        var data = ev.dataTransfer.getData("text");
+        ev.target.appendChild(document.getElementById(data));
+    }
+}
